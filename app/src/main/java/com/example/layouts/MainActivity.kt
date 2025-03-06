3e33a92894413d31e89fc7d7d2c39daf7630d517
DEV <pattnaikd833@gmail.com>
2025-03-06 19:20:23 +0530

    backend is written

--- app/src/main/java/com/example/layouts/MainActivity.kt
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.layouts
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+         val submit: Button =findViewById(R.id.submitButton)
+        val user: EditText =findViewById(R.id.username)
+
+        submit.setOnClickListener {
+            val username = user.text.toString()
+            if (username.isEmpty()) {
+                Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Welcome $username", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
 
 
     }
